9ad78248c9fe9abc8fdaaf5401b796cf9a520947
vitams <vmpartner@gmail.com>
2019-09-28 03:26:12 +0500

    Cron

--- Dockerfile
+++ Dockerfile
@@ -8,9 +8,6 @@ ENV POSTGRES_PASSWORD **None**
 ENV SCHEDULE **None**
 
 ADD backup.sh /backup.sh
-ADD install.sh /install.sh
 ADD run.sh /run.sh
-
-RUN sh install.sh
 
 CMD ["sh", "run.sh"]
--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@ set -e
 if [ "${SCHEDULE}" = "**None**" ]; then
   exit
 else
-  echo "$POSTGRES_PASSWORD" > /root/.pgpass && chmod 0600 /root/.pgpass
-  exec go-cron "$SCHEDULE" /bin/sh backup.sh
+  echo "$POSTGRES_PASSWORD" > /root/.pgpass &&
+  chmod 0600 /root/.pgpass && \
+  echo "$SCHEDULE /backup.sh" > /crontab.txt && \
+  /usr/bin/crontab /crontab.txt && \
+  exec /usr/sbin/crond -f -l 8
 fi
