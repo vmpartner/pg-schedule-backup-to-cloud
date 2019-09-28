eeb606153c2643dba8303636a4468e252d9a5146
vitams <vmpartner@gmail.com>
2019-09-28 14:33:16 +0500

    Cron

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ set -e
 if [ "${SCHEDULE}" = "**None**" ]; then
   exit
 else
-  echo "$POSTGRES_PASSWORD" > /root/.pgpass &&
+  echo "$POSTGRES_HOST:$POSTGRES_PORT:$POSTGRES_DATABASE:$POSTGRES_USER:$POSTGRES_PASSWORD" > /root/.pgpass &&
   chmod 0600 /root/.pgpass && \
   echo "$SCHEDULE /backup.sh" > /crontab.txt && \
   /usr/bin/crontab /crontab.txt && \
