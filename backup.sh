b557ac3e1b8979949aebd4280c0f5d58681507f0
vitams <vmpartner@gmail.com>
2021-05-17 14:45:37 +0500

    No start with exist file

--- backup.sh
+++ backup.sh
@@ -3,6 +3,7 @@
 set -e
 
 mkdir -p /backup
+rm /backup/dump.sql
 pg_dump --create --file=/backup/dump.sql --format=c --dbname="$POSTGRES_DATABASE" --username="$POSTGRES_USER" --host="$POSTGRES_HOST" --port="$POSTGRES_PORT" -v
 sh /upload.sh
 if [ "$CHECK_URL" = "**None**" ]; then
--- upload.sh
+++ upload.sh
@@ -5,6 +5,7 @@ set -e
 DUMPNAME=$(date +%Y%m%d%H%M).sql.gz
 
 cd /backup/
+rm dump.sql.gz
 gzip dump.sql
 mv dump.sql.gz "$DUMPNAME"
 rclone copy "$DUMPNAME" "$RCLONE_DEST"
